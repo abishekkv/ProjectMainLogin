a9ee78e3a20d9c8e10c43dd72ca891b74239829d
Sindhu Sasi <Sindhu Sasi@SINDHUSASIPC>
2022-07-04 13:36:25 +0530

    Add project files.

---
+++ ProjectMainLogin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectMainLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
